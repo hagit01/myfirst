ac5440e91f8aa1d54c116d000137747ad9942f10
hadang <hadh3101199@gmail.com>
2021-07-06 01:13:24 +0700

    'add_html'

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('/detail/<int:question_id>', views.detailview, name='detail'),
     path('/list', views.viewlist, name="view_list"),
     path('', views.index, name="index"),
-    path('/<int:question_id>', views.vote, name='vote'),
+    path('/<int:question_id>/', views.vote, name='vote'),
 ]
