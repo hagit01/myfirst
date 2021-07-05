d444b9eaf6360a76863a23118b5c813db9306d27
hadang <hadh3101199@gmail.com>
2021-07-05 20:39:54 +0700

    abc

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     time_pub = models.DateTimeField()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
